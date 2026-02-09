2ba0345761ffb5721d27391570a046ac54cd6d34
Adan694 <ashehzad0100@gmail.com>
2026-02-09 11:39:23 +0500

    changes pushed

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();  
+
 const express = require('express');
 const http = require('http');             
 const { Server } = require('socket.io');
@@ -26,7 +28,7 @@ const session = require('express-session');
 const app = express();
 const port = process.env.PORT || 3000;
 
-require('dotenv').config();  
+// require('dotenv').config();  
 // const server = http.createServer(app);
 // const io = new Server(server, {
 //   cors: {
